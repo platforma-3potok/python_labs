a74ab1d4c367f7ddbbdf2e65a9959224e1ea9cdd
Kirill <kirill.isheykin@gmail.com>
2025-10-04 18:47:24 +0300

    aaf

---
+++ src/lab03/text.py
@@ -0,0 +1,55 @@
+import string
+def normalize(text: str, *, casefold: bool = True, yo2e: bool = True) -> str:
+    s = text
+    if casefold:
+        s = s.lower()
+    if yo2e:
+        s = s.replace('ё', 'е').replace('Ё', 'Е')
+    s = s.replace('\n', ' ').replace('\r', ' ').replace('\t', ' ')
+    s = s.strip()
+    while '  ' in s:
+        s = s.replace('  ', ' ')
+    return s
+
+def tokenize(text: str) -> list[str]:
+    s = text
+    cyrillic_lower_letters = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+    alp = cyrillic_lower_letters + string.ascii_lowercase + string.digits
+    for i in range(len(s)):
+        if s[i] in alp:
+            continue
+        else:
+            s = s[:i] + ' ' + s[i+1:]
+    while '  ' in s:
+        s = s.replace('  ', ' ')
+    s = s.split()
+    return s
+
+def count_freq(tokens: list[str]) -> dict[str, int]:
+    d = {}
+    tokens_set = set(tokens)
+    for key in tokens_set:
+        d[key] = tokens.count(key)
+    return d
+
+def top_n(freq: dict[str, int], n: int = 5) -> list[tuple[str, int]]:
+    d = freq
+    d = sorted(d.items(), key = lambda para: (-para[1], para[0]))
+    return d
+
+
+example_normalize = "  двойные   пробелы  "
+example_tokenize = "emoji 😀 не слово"
+example_count_freq = ["bb","aa","bb","aa","cc"]
+
+res_normalise = normalize(example_normalize)
+res_tokenize = tokenize(example_tokenize)
+res_count_freq = count_freq(example_count_freq)
+res_count_top_n = top_n(res_count_freq)
+
+print(res_normalise)    
+print(res_tokenize)
+print(res_count_freq)
+print(res_count_top_n)
+        
+
